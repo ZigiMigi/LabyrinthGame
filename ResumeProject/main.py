861dade9e6f4e7c357f037fee6c70ca86af3975f
zigakerzan <ziga.kerzan@gmail.com>
2023-11-14 08:22:08 +0200

    Analiza

--- ResumeProject/main.py
+++ ResumeProject/main.py
@@ -1,14 +1,19 @@
 import sys
+import time
+
 import pygame
 import primsAlgorithm
 import mazeSolver
 import saveMaze
 import kruskalsAlgorithm
-
+import binaryTreeAlgorithm
+import recursiveBacktrackingAlgorithm
+import recursiveDivisionAlgorithm
+import tracemalloc
 
 WIDTH = 600
 HEIGHT = 600
-MAZE_WIDTH = MAZE_HEIGHT = 25
+MAZE_WIDTH = MAZE_HEIGHT = 50
 WALL_THICKNESS = WIDTH // MAZE_WIDTH
 
 
@@ -17,7 +22,28 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Labrynth solver")
-game = kruskalsAlgorithm.kuskalInit(MAZE_WIDTH, MAZE_HEIGHT)
+
+#tracemalloc.start()
+all_times = 0
+all_space = 0
+all_peaks = 0
+for x in range(10):
+    print(f"Iteration: {x}")
+    start_time = time.time()
+    tracemalloc.start()
+    game = recursiveDivisionAlgorithm.recursiveDivisionInit(MAZE_WIDTH, MAZE_HEIGHT)
+    current, peak = tracemalloc.get_traced_memory()
+    end_time = time.time()
+    all_times += (end_time - start_time) * 1000
+    all_space += current
+    all_peaks += peak
+
+print(f"Average elapsed time: {all_times/10:.2f} ms")
+print(f"Average memory usage: {(all_space/10) / 10 ** 6} MB")
+print(f"Average peak memory usage: {(all_peaks/10) / 10 ** 6} MB")
+
+# tracemalloc.stop()
+
 #solution = mazeSolver.solveMaze(game, MAZE_HEIGHT, MAZE_WIDTH)
 saveMaze.writeToFile(game)
 #saveMaze.writeSolution(solution, MAZE_HEIGHT, MAZE_WIDTH)
